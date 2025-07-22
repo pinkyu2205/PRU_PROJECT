937241613ee7a31116401d974dbdc56c6b21ee8f
Nguyễn Hoàng Long 40 <longnhse172340@fpt.edu.vn>
2025-07-22 21:43:10 +0700

    add MainmenuUI

---
+++ Assets/Scripts/SlingShotHandler.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using DG.Tweening;
+
+public class SlingShotHandler : MonoBehaviour
+{
+    [Header("Line Renderers")]
+    [SerializeField] private LineRenderer _leftLineRenderer;
+    [SerializeField] private LineRenderer _rightLineRenderer;
+
+    [Header("Transform References")]
+    [SerializeField] private Transform _leftStartPosition;
+    [SerializeField] private Transform _rightStartPosition;
+    [SerializeField] private Transform _centerPosition;
+    [SerializeField] private Transform _idlePosition;
+    [SerializeField] private Transform _elasticTransform;
+
+    [Header("Slingshot Stats")]
+    [SerializeField] private float _maxDistance = 3.5f;
+    [SerializeField] private float _shotForce = 5f;
+    [SerializeField] private float _timeBetweenBirdRespawns = 2f;
+    [SerializeField] private float _elasticDivider = 1.2f;
+    [SerializeField] private AnimationCurve _elasticCurve;
+    [SerializeField] private float _maxAnimationTime = 1f;
+
+    [Header("Scripts")]
+    [SerializeField] private SlingShotArea _slingShotArea;
+    [SerializeField] private CameraManager _cameraManager;
+
+    [Header("Bird")]
+    [SerializeField] private AngieBird _angryBirdPrefab;
+    [SerializeField] private float _angryBirdPositionOffset = 2f;
+
+    [Header("Sound")]
+    [SerializeField] private AudioClip _elasticPulledClip;
+    [SerializeField] private AudioClip[] _elasticReleasedClip;
+
+    private Vector2 _slingShotLinesPosition;
+    private Vector2 _direction;
+    private Vector2 _directionNormalized;
+
+    private bool _clickedWithinArea;
+    private bool _birdOnSlingshot;
+
+    private AngieBird _spawnAngieBird;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        _leftLineRenderer.enabled = false;
+        _rightLineRenderer.enabled = false;
+
+        SpawnAngieBird();
+    }
+
+    private void Update()
+    {
+        if (InputManager.WasLeftMousebuttonPressed && _slingShotArea.IsWithinSlingshotArea())
+        {
+            _clickedWithinArea = true;
+
+            if (_birdOnSlingshot)
+            {
+                SoundManager.instance.PlayClip(_elasticPulledClip, _audioSource);
+                _cameraManager.SwitchToFollowCam(_spawnAngieBird.transform);
+            }
+        }
+
+        if (InputManager.IsLeftMousePressed && _clickedWithinArea && _birdOnSlingshot)
+        {
+            DrawSlingShot();
+            PositionAndRotationAngieBird();
+        }
+
+        if (InputManager.WasLeftMousebuttonReleased && _birdOnSlingshot && _clickedWithinArea)
+        {
+            if (GameManager.instance.HasEnoughShots())
+            {
+                _clickedWithinArea = false;
+                _birdOnSlingshot = false;
+
+                _spawnAngieBird.LaunchBird(_direction, _shotForce);
+
+                SoundManager.instance.PlayRandomClip(_elasticReleasedClip, _audioSource);
+
+                GameManager.instance.UseShot();
+                AnimateSlingshot();
+            
+
+                if (GameManager.instance.HasEnoughShots())
+                {
+                    StartCoroutine(SpawnAngieBirdAfterTime());
+                }
+                else
+                {
+ 
+                    GameManager.instance.CheckForLastShot();
+                }
+            }
+        }
+    }
+
+    #region SlingShot Methods
+    private void DrawSlingShot()
+    {
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(InputManager.MousePosition);
+
+        _slingShotLinesPosition = _centerPosition.position + Vector3.ClampMagnitude(touchPosition - _centerPosition.position, _maxDistance);
+
+        SetLines(_slingShotLinesPosition);
+
+        _direction = (Vector2)_centerPosition.position - _slingShotLinesPosition;
+        _directionNormalized = _direction.normalized;
+    }
+
+    private void SetLines(Vector2 position)
+    {
+        if (!_leftLineRenderer.enabled && !_rightLineRenderer.enabled)
+        {
+            _leftLineRenderer.enabled = true;
+            _rightLineRenderer.enabled = true;
+        }
+
+        _leftLineRenderer.SetPosition(0, position);
+        _leftLineRenderer.SetPosition(1, _leftStartPosition.position);
+
+        _rightLineRenderer.SetPosition(0, position);
+        _rightLineRenderer.SetPosition(1, _rightStartPosition.position);
+    }
+    #endregion
+
+    #region Angie Bird Methods
+    private void SpawnAngieBird()
+    {
+        _elasticTransform.DOComplete();
+        SetLines(_idlePosition.position);
+
+        Vector2 dir = (_centerPosition.position - _idlePosition.position).normalized;
+        Vector2 spawnPosition = (Vector2)_idlePosition.position + dir * _angryBirdPositionOffset;
+
+        _spawnAngieBird = (AngieBird)Instantiate(_angryBirdPrefab, spawnPosition, Quaternion.identity);
+        _spawnAngieBird.transform.right = dir;
+
+        _birdOnSlingshot = true;
+    }
+
+    private void PositionAndRotationAngieBird()
+    {
+        _spawnAngieBird.transform.position = _slingShotLinesPosition + _directionNormalized * _angryBirdPositionOffset;
+        _spawnAngieBird.transform.right = _directionNormalized;
+    }
+
+    private IEnumerator SpawnAngieBirdAfterTime()
+    {
+        yield return new WaitForSeconds(_timeBetweenBirdRespawns);
+
+        SpawnAngieBird();
+
+        _cameraManager.SwitchToIdleCam();
+    }
+    #endregion
+    #region Animate Slingshot
+    private void AnimateSlingshot()
+    {
+        _elasticTransform.position = _leftLineRenderer.GetPosition(0);
+
+        float dist = Vector2.Distance(_elasticTransform.position, _centerPosition.position);
+
+        float time = dist / _elasticDivider ;
+
+        _elasticTransform.DOMove(_centerPosition.position, time).SetEase(_elasticCurve);
+        StartCoroutine(AnimteSlingshotLines(_elasticTransform, time));
+    }
+
+    private IEnumerator AnimteSlingshotLines(Transform trans, float time)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < time && elapsedTime < _maxAnimationTime)
+        {
+            elapsedTime += Time.deltaTime;
+
+
+            SetLines(trans.position);
+
+            yield return null;
+        }
+    }
+
+    #endregion
+}
